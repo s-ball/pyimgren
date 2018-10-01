a4f884b8bae126292a16cd20ff55b421cca768de
s-ball <s-ball@laposte.net>
2018-10-01 21:17:43 +0200

    Prepare initial structure of the project.

---
+++ pyimgren/pyimgren.py
@@ -0,0 +1,99 @@
+import os.path
+import glob
+import datetime
+import piexif
+import csv
+import sys
+import collections
+
+class Converter:
+    """A Converter is used to convert image names provided by a camera
+(commonly IMGxxxxx.JPG or DSCFyyyy.JPG into a name based on the time
+when the photography had been taken (as smartphones do). It is then
+possible to rename them to their original name.
+
+A file named names.log is created in the folder to store the new names
+and the original ones, and is what allows to rename them back.
+
+Typical use:
+
+conv = Converter(path)
+conv.convert()    # to convert to "date" names
+conv.back()
+
+This class requires piexif and Python >= 3."""
+    
+    def __init__(self, folder, src_mask = "DSCF*.jpg",
+                 dst_mask = "%Y%m%d_%H%M%S",
+                 ext_mask = ".jpg",
+                 ref_file = "names.log",
+                 debug = False,
+                 dummy = False):
+        self.folder, self.src_mask, self.dst_mask, self.ref_file = (
+            folder, src_mask, dst_mask, ref_file)
+        self.ext_mask, self.debug, self.dummy = ext_mask, debug, dummy
+    def convert(self, folder = None):
+        if folder is None: folder = self.folder
+        orig_folder = os.getcwd()
+        os.chdir(folder)
+        names = self._load_names()
+        for file in glob.glob(self.src_mask):
+            dat = self._get_dat(file)
+            if dat is not None:
+                new_name = self._get_new_name(
+                    datetime.datetime.strftime(dat, self.dst_mask)) \
+                     + self.ext_mask
+                if self.debug:
+                    print(file, "->", new_name, file=sys.stderr)
+                names[new_name] = file
+                if not self.dummy:
+                    os.rename(file, new_name)
+        if len(names) != 0:
+            with open(self.ref_file, "w", newline='') as fd:
+                wr = csv.writer(fd, delimiter = ":")
+                for name, old in names.items():
+                    wr.writerow((name, old))
+        os.chdir(orig_folder)
+    def back(self, folder = None):
+        if folder is None: folder = self.folder
+        orig_folder = os.getcwd()
+        os.chdir(folder)
+        names = self._load_names()
+        for name, orig in names.items():
+            if self.debug: print(name, "->", orig, file=sys.stderr)
+            if not self.dummy: os.rename(name, orig)
+        os.chdir(orig_folder)
+    def _load_names(self):
+        names = collections.OrderedDict()
+        try:
+            with open(self.ref_file) as fd:
+                rd = csv.reader(fd, delimiter=":")
+                for numlig, row in enumerate(rd):
+                    names[row[0]] = row[1]
+        except FileNotFoundError:
+            pass
+        except IndexError:
+            print(numlig, row)
+            raise
+        return names
+    def _get_dat(self, file):
+        exif = piexif.load(file)["Exif"]
+        dt = None
+        for i in (0x9003, 0x9004):
+            dt = exif[i]
+            if dt is not None: break
+        if dt is None: return None
+        return datetime.datetime.strptime(dt.decode('ascii'),
+                                          "%Y:%m:%d %H:%M:%S")
+    def _get_new_name(self, name):
+        if os.path.exists(name + self.ext_mask):
+            for i in range(ord('a'), ord('z') + 1):
+                n = name + chr(i)
+                if not os.path.exists(n + self.ext_mask): return n
+            for i in range(ord('a'), ord('z') + 1):
+                for j in range(ord('a'), ord('z') + 1):
+                    n = name + chr(i) + chr(j) + self.ext_mask
+                    if not os.path.exist(n): return n
+            raise RuntimeError("Too much files for {}".format(
+                name + self.ext_mask))
+        return name
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+from io import open
+
+with open('README.md') as fd:
+    long_description = fd.read()
+
+setup(
+    name="pyimgren",
+    version = "0.1.0",
+    description = "Rename image files according to exif tags",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages = find_packages(ignore = ["tests", "docs"]),
+    install_requires = ["piexif"],
+    author="s-ball",
+    author_email = 's-ball@laposte.net',
+    url = "https://github.com/s-ball/pyimgren",
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+    python_requires=">=3",
+    entry_points = {
+        'console_scripts': [
+            'pyimgren=pyimgren:main';
+            ],
+        },
+    )
