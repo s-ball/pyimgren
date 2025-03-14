6f6007fc35adc59823c412adc6d7250881bf02c5
s-ball <s-ball@laposte.net>
2025-03-14 12:16:18 +0100

    Clean up __init__.py
    Remove the deprecated getdefaultlocale and clean up the special __all__ variable from unwanted names.

--- pyimgren/__init__.py
+++ pyimgren/__init__.py
@@ -1,25 +1,29 @@
 # MIT License
-# Copyright (c) 2018 s-ball
+# Copyright (c) 2018-current s-ball
 
 from .pyimgren import Renamer
 from .version import version as __version__
+
 # import .pyimgren
 
-__all__ = ["Renamer", "__version__", "__name__"]
+__all__ = ["Renamer"]
 
 import gettext
 import locale
 import os.path
+
+print(__name__)
 
 def nls_init(reset=False):
     if reset:
         pyimgren._ = lambda x: x
     else:
         loc = locale.getlocale()[0]
-        if loc is None: loc = locale.getdefaultlocale()[0]
+        if loc is None:
+            loc = locale.setlocale(locale.LC_ALL, '')[0]
 
         pyimgren._ = gettext.translation("pyimgren",
-                                os.path.join(os.path.dirname(__file__),
-                                             "locale"),
-                                loc,
-                                fallback=True).gettext
+                                         os.path.join(os.path.dirname(__file__),
+                                                      "locale"),
+                                         loc,
+                                         fallback=True).gettext
